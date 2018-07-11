dd428f6bdc6aa246c0de2f0ead74e723cbe680f3
panicoenlaxbox <panicoenlaxbox@gmail.com>
2018-07-11 23:58:36 +0200

    Add project files.

---
+++ ClassLibrary1/Controllers/FooController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassLibrary1
+{
+    [Route("api/{controller}")]
+    public class FooController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string[]> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
